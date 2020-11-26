3d253430d339f4472a9e0a0e954aabf7a459daae
Henrique Silva Chacorowski <henrique.ti@grupointegrado.br>
2020-11-25 22:20:58 -0300

    Adicionando a lib bodyParser para ler requisicoes urlencoded/json

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -1,4 +1,10 @@
 //recebe o app e exporta 
 module.exports = app => {
     app.get('/contatos', (req, res) => res.send('Servidor rodando, tudo ok'));
+
+    app.post('/contatos', (req, res) => {
+        console.log(req.body);
+        res.send('Post rodando ok')
+    });
+
 }
