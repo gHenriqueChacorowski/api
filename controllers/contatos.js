d329f1b5e14c7b7b6e183390120eb83369d3edf3
Henrique Silva Chacorowski <henrique.ti@grupointegrado.br>
2020-11-25 23:34:35 -0300

    Crud da classe Contato e validacao de erro no metodo adiciona

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -2,13 +2,34 @@
 const Contato = require("../models/contatos");
 
 module.exports = app => {
-    app.get('/contatos', (req, res) => res.send('Servidor rodando, tudo ok'));
+    app.get('/contatos', (req, res) => {
+        Contato.lista(res);
+        res.send('Listando');
+    });
+
+    app.get('/contatos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Contato.listaId(id, res);
+    })
 
     app.post('/contatos', (req, res) => {
-        const contato = req.body
+        const contato = req.body;
 
-        Contato.adiciona(contato);
-        res.send('Post rodando ok')
+        Contato.adiciona(contato, res);
     });
+
+    app.patch('/contatos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const values = req.body;
+
+        Contato.altera(id, values, res);
+    })
+
+    app.delete('/contatos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Contato.deleta(id, res);
+    })
 
 }
--- models/contatos.js
+++ models/contatos.js
@@ -1,14 +1,78 @@
 const conexao = require('../infraestrutura/conexao');
 
 class Contato {
-    adiciona(contato) {
-        const sql = "INSERT INTO contatos SET ?"
+    adiciona(contato, res) {
+        const nomeEhValido = contato.nome.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'nome',
+                valido: nomeEhValido,
+                mensagem: 'Nome deve ter no minimo 5 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+
+        if(erros.length){
+            res.status(400).json(erros);
+        } else {
+            const sql = "INSERT INTO contatos SET ?"
         
-        conexao.query(sql, contato, (erro, resultados) => {
+            conexao.query(sql, contato, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro);
+                } else {
+                    res.status(200).json(resultados);
+                }
+            })
+        } 
+    }
+
+    lista(res){
+        const sql = "SELECT * FROM contatos"
+
+        conexao.query(sql, (erro, resultados) => {
             if(erro){
-                console.log(erro);
+                res.status(400).json(erro);
             } else {
-                console.log(resultados);
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    listaId(id, res){
+        const sql = "SELECT * FROM contatos WHERE id = ?"
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    deleta(id, res){
+        const sql = "DELETE FROM contatos WHERE id = ?"
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    altera(id, values, res){
+        const sql = "UPDATE contatos SET ? WHERE id = ?"
+
+        conexao.query(sql, [values, id], (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
             }
         })
     }
